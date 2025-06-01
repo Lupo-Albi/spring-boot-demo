fa01ad8e775a6dbc613eb4cc39d85d02d1a986e8
Luanderson Martins <43974046+Lupo-Albi@users.noreply.github.com>
2025-06-01 16:31:58 -0300

    ✨ Created POST and GET by id routes for softwareEngineer

--- src/main/java/dev/luanderson/SoftwareEngineerController.java
+++ src/main/java/dev/luanderson/SoftwareEngineerController.java
@@ -1,9 +1,14 @@
 package dev.luanderson;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+
+/*
+ It's not recommended to expose entities directly in methods, as this may leak sensitive data.
+ In a real application, we would create DTOs (Data Transfer Objects) to send and receive only the required properties.
+ Since this project is for study purposes, I haven't implemented DTOs at this time.
+*/
 
 @RestController
 @RequestMapping("api/v1/software-engineers")
@@ -19,4 +24,18 @@ public class SoftwareEngineerController {
     public List<SoftwareEngineer> getAllSoftwareEngineers() {
         return softwareEngineerService.getAllSoftwareEngineers();
     }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getSoftwareEngineerById(@PathVariable Integer id) {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
+    }
+
+    // TODO: implement the feature to delete software engineer by id
+
+    // TODO: implement the feature to update software engineer by id
 }
--- src/main/java/dev/luanderson/SoftwareEngineerService.java
+++ src/main/java/dev/luanderson/SoftwareEngineerService.java
@@ -2,6 +2,7 @@ package dev.luanderson;
 
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SoftwareEngineerService {
@@ -14,5 +15,13 @@ public class SoftwareEngineerService {
 
     public List<SoftwareEngineer> getAllSoftwareEngineers() {
         return softwareEngineerRepository.findAll();
+    }
+
+    public void insertSoftwareEngineer(SoftwareEngineer softwareEngineer) {
+        softwareEngineerRepository.save(softwareEngineer);
+    }
+
+    public SoftwareEngineer getSoftwareEngineerById(Integer id) {
+        return softwareEngineerRepository.findById(id).orElseThrow(() -> new IllegalStateException(id + " not found"));
     }
 }
