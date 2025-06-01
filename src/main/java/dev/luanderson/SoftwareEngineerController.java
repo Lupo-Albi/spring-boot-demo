12f6a0e3871790728b2e4396ef0c1d93bd629eaa
Luanderson Martins <43974046+Lupo-Albi@users.noreply.github.com>
2025-06-01 15:59:09 -0300

    👔Cange SoftwareEngineer logic to fetch from database

--- src/main/java/dev/luanderson/SoftwareEngineerController.java
+++ src/main/java/dev/luanderson/SoftwareEngineerController.java
@@ -3,18 +3,20 @@ package dev.luanderson;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
 
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
     @GetMapping
-    public List<SoftwareEngineer> getSoftwareEngineers() {
-        return List.of(
-                new SoftwareEngineer(1, "Lupo", "js, node, react, mongo, c#, java"),
-                new SoftwareEngineer(2, "Linda", "c#, .net, python, django")
-        );
+    public List<SoftwareEngineer> getAllSoftwareEngineers() {
+        return softwareEngineerService.getAllSoftwareEngineers();
     }
 }
